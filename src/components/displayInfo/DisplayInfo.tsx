026a7269c183b81e47424f9bb4da7c44d0a9b63e
Shubham Deshmukh <dshubham1295@gmail.com>
2022-01-26 20:46:25 +0530

    Fix Css

--- src/components/displayInfo/DisplayInfo.tsx
+++ src/components/displayInfo/DisplayInfo.tsx
@@ -23,24 +23,11 @@ const DisplayInfo = (props: any) => {
                   <p >Release Date : {props.release}</p>
                 
                   <p >{props.description}</p>
-                  {/* {props.description.split(".").map((char: any)=>(
-              <p>{char}</p>
-          ))} */}
+                  
                   <p >{props.genere}</p>
                 </div>
               </div>
-
-
-              {/* <div className="movie-details">
-        <img src={`${baseUrlPoster}${props.image}`} alt={props.name} />
-      </div>
-      <div className="movie-details">
-        <h6>{props.name}</h6>
-        <h6>{props.description}</h6>
-        <h6>{props.id}</h6>
-        <h6>{props.genere}</h6>
-        <h6>{props.release}</h6>
-      </div>*/}
+              
             </div>
           </div>
           <div className="card mobile" style={{
@@ -49,34 +36,18 @@ const DisplayInfo = (props: any) => {
           }}>
             <div className="card-body">
 
-
               <img src={`${baseUrlPoster}${props.image}`} alt={props.name} />
 
-
-              <div>
-                <br></br>
-                <span className="title"><h6>{props.name}</h6></span>
-                <p >{props.release}</p>
-                <br></br>
-                <p >{props.description}</p>
-                {/* {props.description.split(".").map((char: any)=>(
-              <p>{char}</p>
-          ))} */}
-                <p >{props.genere}</p>
-              </div>
+              <div className="col-10">
+                  <span ><h1>{props.name}</h1></span>
+                  <p >Release Date : {props.release}</p>
+                
+                  <p >{props.description}</p>
+                  
+                  <p >{props.genere}</p>
+                </div>
             </div>
 
-
-            {/* <div className="movie-details">
-        <img src={`${baseUrlPoster}${props.image}`} alt={props.name} />
-      </div>
-      <div className="movie-details">
-        <h6>{props.name}</h6>
-        <h6>{props.description}</h6>
-        <h6>{props.id}</h6>
-        <h6>{props.genere}</h6>
-        <h6>{props.release}</h6>
-      </div>*/}
           </div>
         </div>
       </div>
